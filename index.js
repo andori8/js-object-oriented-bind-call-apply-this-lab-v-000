83a9fe1ea58cec32ff366fdbe90fe3fc4af4c6e2
Ryan Do <falcsmike7@gmail.com>
2017-11-06 23:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, args) {
   return fn.call(thisValue, args);
 }
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
+}
